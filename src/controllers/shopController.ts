dc21bb981ccc93bbf51cffe8434c8e203aab6296
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-11 11:06:57 +0700

    feat: add ShopController with method to retrieve shop data

---
+++ src/controllers/shopController.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { ShopService } from "../services/shopService";
+
+export class ShopController {
+  static async getData(req: Request, res: Response, next: NextFunction) {
+      try {
+        const shop = await ShopService.getData();
+        res.json(shop);
+      } catch (error) {
+        next(error);
+      }
+    }
+}
