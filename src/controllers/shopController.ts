387faa36dbf08d442d354b00781c98dfcb8da779
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-12 10:34:33 +0700

    refactor: improve formatting in ShopController's getData method

--- src/controllers/shopController.ts
+++ src/controllers/shopController.ts
@@ -3,11 +3,11 @@ import { ShopService } from "../services/shopService";
 
 export class ShopController {
   static async getData(req: Request, res: Response, next: NextFunction) {
-      try {
-        const shop = await ShopService.getData();
-        res.json(shop);
-      } catch (error) {
-        next(error);
-      }
+    try {
+      const shop = await ShopService.getData();
+      res.json(shop);
+    } catch (error) {
+      next(error);
     }
+  }
 }
