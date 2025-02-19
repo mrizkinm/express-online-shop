6a70590ab012a9044d2fef66c1b81d863967982b
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-19 09:54:04 +0700

    add middleware to remove updatedAt field from query results

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -2,4 +2,22 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+prisma.$use(async (params, next) => {
+  const result = await next(params);
+
+  // Hapus updatedAt jika hasilnya berupa array atau objek tunggal
+  if (Array.isArray(result)) {
+    return result.map((item) => {
+      if (item.updatedAt) delete item.updatedAt;
+      return item;
+    });
+  }
+
+  if (result && result.updatedAt) {
+    delete result.updatedAt;
+  }
+
+  return result;
+});
+
 export default prisma;
