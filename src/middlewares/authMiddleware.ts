790b162decec406634ad61aeb79d885b58a1f879
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-19 09:54:41 +0700

    refactor authMiddleware to use 'errors' key in response and update token retrieval from user to customer

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -6,12 +6,12 @@ const authMiddleware = async (req: Request, res: Response, next: NextFunction) =
   const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) {
-    res.status(401).json({ message: "Unauthorized: No token provided" });
+    res.status(401).json({ errors: "Unauthorized: No token provided" });
     return;
   }
 
   try {
-    const storedToken  = await prisma.user.findFirst({
+    const storedToken  = await prisma.customer.findFirst({
       where: { token: token },
     });
 
@@ -24,7 +24,7 @@ const authMiddleware = async (req: Request, res: Response, next: NextFunction) =
     (req as any).user = decoded; // Menyimpan user yang terautentikasi di request
     next();
   } catch (error) {
-    res.status(401).json({ message: "Unauthorized: Invalid token" });
+    res.status(401).json({ errors: "Unauthorized: Invalid token" });
     return;
   }
 };
