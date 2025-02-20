eabef0d9588413510430dfc580513e523c5f57f4
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-20 09:42:48 +0700

    add Swagger documentation for shop routes

--- src/routes/shopRoute.ts
+++ src/routes/shopRoute.ts
@@ -3,5 +3,60 @@ import { ShopController } from "../controllers/shopController";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/shop:
+ *   get:
+ *     summary: Get shop information
+ *     description: Get shop data information
+ *     tags:
+ *       - Shop
+ *     responses:
+ *       200:
+ *         description: Shop information
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   example: "example shop"
+ *                 address:
+ *                   type: string
+ *                   example: "example address"
+ *                 phone:
+ *                   type: string
+ *                   example: "example phone"
+ *                 email:
+ *                   type: string
+ *                   example: "example email"
+ *                 description:
+ *                   type: string
+ *                   example: "example description"
+ *                 image:
+ *                   type: string
+ *                   example: "http://test.io/test.png"
+ *       401:
+ *         description: Invalid token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: string
+ *                   example: "Unauthorized: Invalid token"
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: string
+ *                   example: "Something went wrong"
+ */
 router.get("/", ShopController.getData);
 export default router;
