9b93eb0fb4ce142f50de8236f6ffaece26f9684b
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-27 09:50:46 +0700

    add rate limiting middleware to control request frequency

--- src/middlewares/limiterMiddleware.ts
+++ src/middlewares/limiterMiddleware.ts
@@ -3,7 +3,7 @@ import rateLimit from "express-rate-limit";
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 menit
   max: 100, // Maks 100 request per IP dalam 15 menit
-  message: "Terlalu banyak request, coba lagi nanti.",
+  message: { errors: 'Too many requests, please try again later.' },
 });
 
 export default limiter;
