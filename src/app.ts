c4d847aca3aa3d0667a86d90ab699064c9be3da5
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-02-28 09:57:40 +0700

    refactor middleware imports and add request logging

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import userRoute from "./routes/userRoute";
 import cartRoute from "./routes/cartRoute";
@@ -6,13 +6,13 @@ import categoryRoute from "./routes/categoryRoute";
 import productRoute from "./routes/productRoute";
 import shopRoute from "./routes/shopRoute";
 import orderRoute from "./routes/orderRoute";
-import loggerMiddleware from "./middlewares/loggerMiddleware";
 import errorMiddleware from "./middlewares/errorMiddleware";
-import limiter from "./middlewares/limiter";
+import limiter from "./middlewares/limiterMiddleware";
 import helmet from "helmet";
 import xssClean from "xss-clean";
 import sanitizeMiddleware from "./middlewares/sanitizeMiddleware";
 import { setupSwagger } from "./config/swagger";
+import { logger } from "./utils/logger";
 
 const app = express();
 setupSwagger(app);
@@ -22,9 +22,13 @@ app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(xssClean());
 app.use(sanitizeMiddleware);
 app.use(express.json());
-app.use(loggerMiddleware);
 app.use(limiter);
 app.use(helmet());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use("/api/user", userRoute);
@@ -36,6 +40,16 @@ app.use("/api/order", orderRoute);
 
 // Error Handling Middleware
 app.use(errorMiddleware);
+
+// Route dengan error handling
+app.get('/error', (req, res) => {
+  try {
+    throw new Error('Simulated error');
+  } catch (err) {
+    logger.error('Error occurred', { error: err });
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(403).json({ errors: "Forbidden" });
